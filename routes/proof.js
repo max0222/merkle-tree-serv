73a6f7fa29417b85a0d47c3a8998187369b8df87
Max <18067886+max0222@users.noreply.github.com>
2023-12-13 18:48:54 +0800

    format res

--- routes/proof.js
+++ routes/proof.js
@@ -5,19 +5,19 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {
   const data = req.body;
-  if (!Array.isArray(data.addrs)) {
+  if (!Array.isArray(data)) {
     res.send({
       error: 'invalid params'
     });
   }
-  const leaves = data.addrs
+  const leaves = data
       .map((addr) => ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['address'], [ethers.utils.getAddress(addr)])));
   const tree = new MerkleTree(leaves, ethers.utils.keccak256, { sortPairs: true });
   const proofs = []
-  for (let i = 0; i < data.addrs.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     proofs.push({
-      address: data.addrs[i],
-      proof: tree.getHexProof(leaves[i]),
+      address: data[i],
+      proof: JSON.stringify(tree.getHexProof(leaves[i])),
     })
   }
 
